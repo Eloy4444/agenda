5629738f9abd73861ba2c0443a1a538335e94d14
Arturo Cordero <vakmancordero@gmail.com>
2019-11-10 13:18:32 -0600

    adding fullcalendar interaction and adding eventClick and dateClick events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+import {EventInput} from "@fullcalendar/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'agenda';
 
-  calendarPlugins = [dayGridPlugin]; // important!
+  calendarEvents: EventInput[];
+  calendarPlugins = [dayGridPlugin, interactionPlugin]; // important!
+
+  dateClick(event) {
+    console.log("event", event);
+  }
+
+  ngOnInit(): void {
+
+    this.calendarEvents = [
+      {
+        title: 'Event Now',
+        extendedProps: {
+          reservationId: 12
+        },
+        start: new Date()
+      }
+    ];
+
+  }
 
 }
